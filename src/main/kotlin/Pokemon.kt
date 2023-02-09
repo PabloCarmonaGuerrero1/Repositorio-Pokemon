7c91c9274c0abadf08f6717b5749e95ad7b2a1db
Pablo Carmona Guerrero <pcargue233@g.educaand.es>
2023-02-09 16:39:08 +0100

    Resolución del ticket 0002

--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -1,3 +1,23 @@
 class Pokemon(id:Int,nombre:String,vida:Int,tipo:String) {
+    var id:Int = 0
+        set(value){
+            if(id<0){
+                throw Exception("El ID no puede ser menor que 0")
+            }
+            field=value
+        }
+    var nombre:String = ""
+    var vida:Int = 0
+    var tipo:String=""
+    init{
+        this.id = id;
+        this.nombre=nombre;
+        this.vida=vida;
+        this.tipo = tipo
 
+    }
+
+    override fun toString(): String {
+        return "Hola mi nombre es $nombre, y tengo $vida de vida y soy de tipo $tipo"
+    }
 }
