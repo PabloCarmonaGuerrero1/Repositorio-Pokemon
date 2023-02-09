2ad91cd4e036f308dcdb4a0c7470398e5c48c261
Pablo Carmona Guerrero <pcargue233@g.educaand.es>
2023-02-09 17:24:45 +0100

    cambio rápido

--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -1,13 +1,14 @@
 class Pokemon(id:Int,nombre:String,vida:Int,tipo:String) {
     var id:Int = 0
         set(value){
-            if(id<0){
+            if(id<=0){
                 throw Exception("El ID no puede ser menor que 0")
             }
             field=value
         }
     var nombre:String = ""
     var vida:Int = 0
+
     var tipo:String=""
     init{
         this.id = id;
