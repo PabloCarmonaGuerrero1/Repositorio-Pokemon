5b4f72f22bdcc2923918ddfc51603eebc3cf1afa
Pablo Carmona Guerrero <pcargue233@g.educaand.es>
2023-02-22 19:32:39 +0100

    He hecho una lista de los tipos de pokémon y un set para que el programa lance un error cuando no sea del tipo correcto , y lo reciba cuando sea un tipo correcto

--- src/main/kotlin/Tipo.kt
+++ src/main/kotlin/Tipo.kt
@@ -1,5 +1,20 @@
 class tipo(Tipo:String){
+    var tipos = mutableListOf<String>("acero","agua","bicho","dragon","electrico","fantasma","fuego"
+        ,"hada","hielo","lucha","normal","planta","psíquico","roca","siniestro","tierra","veneno","volador")
+
     var tipo = ""
+        get(){
+            return field
+        }
+        set(value){
+            if (value !in tipos){
+                IllegalArgumentException("Este tipo no es válido")
+            }
+            else{
+                field=value
+            }
+        }
+
     init{
         tipo = Tipo
     }
